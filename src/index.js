271ffe8c49613b7ff90252d9871907f9b15268ee
speakingcode <dlissner@gmail.com>
2019-04-01 14:55:53 -0500

    Add redux devtools, create middleware to transport actions via socket.io, and reducer to set local state

--- src/index.js
+++ src/index.js
@@ -1,30 +1,62 @@
 import React    from 'react'
 import ReactDOM from 'react-dom'
 
-import { createStore } from 'redux'
+import {
+  createStore,
+  applyMiddleware,
+  compose
+}                            from 'redux'
 
 import io from 'socket.io-client'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
-  (state = {}, action) => action.newState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  (state = {}, action) =>
+    action.type === 'SET_NEW_STATE'
+    ? action.newState
+    : state,
+
+  composeEnhancers(
+    applyMiddleware(
+      store => next => action => {
+        console.log('sending action:', action)
+        if (action.type === 'SET_NEW_STATE') {
+          return next(action)
+        }
+        else {
+          socket.emit('DISPATCH_CLIENT_ACTION', action)
+          return next(action)
+        }
+      }
+    )
+  )
 )
 
 const socket = io.connect('http://localhost:8090')
-socket.on('NEW_STATE', newState => {
-  console.log('NEW STATE', newState)
-  store.dispatch({type: 'NEW_STATE', newState})
+socket.on('SET_NEW_CLIENT_STATE', newState => {
+  console.log('Setting new state from server:', newState)
+  store.dispatch({type: 'SET_NEW_STATE', newState})
 })
 
 
-const App = ({title}) =>
-  <h1>{title}</h1>
+const App = ({state}) =>
+  <div>
+    <h1>App</h1>
 
+    <input
+      value={state}
+      type="text"
+      onChange={(e) => {store.dispatch({type: 'SET_INPUT', payload: e.target.value})}}
+    />
+    <button onClick={() => {store.dispatch({type: 'SUBMIT_INPUT', payload: state})}}>
+      Send action!
+    </button>
+  </div>
 
 store.subscribe(() => {
   ReactDOM.render(
-    <App title="App" />,
+    <App state={store.getState()} />,
     document.getElementById('app')
   )
 })
