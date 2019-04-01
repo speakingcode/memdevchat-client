22ba8727569710e683a324858b442a5dd4fa28a3
speakingcode <dlissner@gmail.com>
2019-04-01 12:17:39 -0500

    Use redux store and devtools to manage client state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React    from 'react'
 import ReactDOM from 'react-dom'
 
+import { createStore } from 'redux'
+
+import io from 'socket.io-client'
+
+
+const store = createStore(
+  (state = {}, action) => action.newState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const socket = io.connect('http://localhost:8090')
+socket.on('NEW_STATE', newState => {
+  console.log('NEW STATE', newState)
+  store.dispatch({type: 'NEW_STATE', newState})
+})
+
+
 const App = ({title}) =>
   <h1>{title}</h1>
 
 
-const render = () => {
+store.subscribe(() => {
   ReactDOM.render(
     <App title="App" />,
     document.getElementById('app')
   )
-}
-
-render()
+})
