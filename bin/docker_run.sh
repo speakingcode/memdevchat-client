04748e3a5291eb92121c000e1fae5d64cd816827
speakingcode <dlissner@gmail.com>
2019-04-02 14:49:19 -0500

    Bind container to correct ports in scripts

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -3,4 +3,4 @@ docker container stop memdevchat-client
 docker container rm memdevchat-client
 
 #run the container with terminal, name it, mount src dir, bind ports
-docker run -t -i --name memdevchat-client -v `pwd`/src:/memdevchat-client/src -p 8090:8090 memdevchat-client npm start
+docker run -t -i --name memdevchat-client -v `pwd`/src:/memdevchat-client/src -p 8080:8080 memdevchat-client npm start
