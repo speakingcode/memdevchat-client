722aab62808a5710b112844f3ebc97aa4695dd49
speakingcode <dlissner@gmail.com>
2019-04-02 09:50:45 -0500

    Configure webpack to load images, styles

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,28 @@ module.exports = {
     './src/index.js'
   ],
   module : {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [
-        'react-hot-loader/webpack',
-        'babel-loader'
-      ]
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'react-hot-loader/webpack',
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader'
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
